7fc4c5636a0086bb9518648eae26b95a996ac75a
kanggiwu <wu10604@gmail.com>
2021-05-20 11:33:20 +0900

    20210520 강지우
    클래스 파일

--- jiwoo_demo/src/main/java/com/example/demo/JiwooDemoApplication.java
+++ jiwoo_demo/src/main/java/com/example/demo/JiwooDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JiwooDemoApplication {
-
+	//테스트
 	public static void main(String[] args) {
 		SpringApplication.run(JiwooDemoApplication.class, args);
 	}
